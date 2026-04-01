1a9e829889d6459589863eae60eed16f831720fc
Huỳnh Trân <quetran.010604@gmail.com>
2026-04-01 08:35:23 +0700

    Update Program.cs

--- SP26SE110_BoneVisQA/BoneVisQA.API/Program.cs
+++ SP26SE110_BoneVisQA/BoneVisQA.API/Program.cs
@@ -173,7 +173,7 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
